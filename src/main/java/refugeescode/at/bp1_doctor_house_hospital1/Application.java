12a57e70d0ab4a49929eb7e78f73b5c40e90d8e6
Thaerohde <thaerohde@gmail.com>
2018-04-08 16:37:37 +0200

    add pom1 file

---
+++ src/main/java/refugeescode/at/bp1_doctor_house_hospital1/Application.java
@@ -0,0 +1,12 @@
+package refugeescode.at.bp1_doctor_house_hospital1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
